ab577c7ce2b65df767367fbc080173373cb1c0c3
Matt Andreko <mandreko@gmail.com>
2012-01-05 08:55:56 -0500

    first commit

---
+++ Skullspace8/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Skullspace8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.skullspaceWebBrowser = new System.Windows.Forms.WebBrowser();
+            this.sequenceNumberLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // skullspaceWebBrowser
+            // 
+            this.skullspaceWebBrowser.Location = new System.Drawing.Point(12, 12);
+            this.skullspaceWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.skullspaceWebBrowser.Name = "skullspaceWebBrowser";
+            this.skullspaceWebBrowser.Size = new System.Drawing.Size(784, 509);
+            this.skullspaceWebBrowser.TabIndex = 0;
+            this.skullspaceWebBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.skullspaceWebBrowser_DocumentCompleted);
+            // 
+            // sequenceNumberLabel
+            // 
+            this.sequenceNumberLabel.AutoSize = true;
+            this.sequenceNumberLabel.Location = new System.Drawing.Point(26, 536);
+            this.sequenceNumberLabel.Name = "sequenceNumberLabel";
+            this.sequenceNumberLabel.Size = new System.Drawing.Size(0, 13);
+            this.sequenceNumberLabel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 578);
+            this.Controls.Add(this.sequenceNumberLabel);
+            this.Controls.Add(this.skullspaceWebBrowser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser skullspaceWebBrowser;
+        private System.Windows.Forms.Label sequenceNumberLabel;
+    }
+}
+
---
+++ Skullspace8/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Skullspace8
+{
+    public partial class Form1 : Form
+    {
+        private bool _hasPosted = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            skullspaceWebBrowser.Navigate("http://www.skullspace.net/2011/08-batman/herecomes9.php");
+
+        }
+
+        private void skullspaceWebBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            // Find the number to set
+            if (skullspaceWebBrowser.Document != null)
+            {
+                if (!_hasPosted)
+                {
+                    HtmlElementCollection strong = skullspaceWebBrowser.Document.GetElementsByTagName("strong");
+                    string strongText = strong[0].InnerText;
+                    string numberString = strongText.Substring(16, strongText.IndexOf(' ', 16) - 16);
+                    sequenceNumberLabel.Text = numberString;
+                    int number = Convert.ToInt32(numberString);
+
+                    HtmlElementCollection inputs = skullspaceWebBrowser.Document.GetElementsByTagName("input");
+
+                    HtmlElement answerInput = inputs.Cast<HtmlElement>().Single(input => input.Name == "answer");
+                    HtmlElement submitButton = inputs.Cast<HtmlElement>().Single(input => input.OuterHtml.Contains("Answer"));
+
+                    Debug.Assert(answerInput != null);
+
+                    answerInput.InnerText = Fibonacci(number).ToString();
+                    submitButton.InvokeMember("click");
+                    _hasPosted = true; 
+                }
+                else
+                {
+                    // since we have posted, search for the phrase "The next level is". If not found, re-post
+                    HtmlElementCollection spans = skullspaceWebBrowser.Document.GetElementsByTagName("span");
+                    if (spans.Count == 0 || !spans[0].InnerHtml.Contains("The next level is "))
+                    {
+                        _hasPosted = false;
+                        skullspaceWebBrowser.Navigate("http://www.skullspace.net/2011/08-batman/herecomes9.php");
+                    }
+                }
+            }
+        }
+
+        //recursive
+        //public int Fibonacci(int n)
+        //{
+        //    if (n == 0 || n == 1)
+        //        return n;
+
+        //    return Fibonacci(n - 1) + Fibonacci(n - 2);
+        //}
+
+        //iterative
+        public static double Fibonacci(int n)
+        {
+            double a = 0;
+            double b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                double temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+    }
+}
